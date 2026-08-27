1790c23ce6f85e5a87cc3801c647a61b872218d4
Ryan Zhuang <ryanzhuang1203@gmail.com>
2026-08-27 12:15:57 -0400

    Today Date debugged

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 function updateClock(){
 
     const now = new Date();
-    const hours = now.getHours().toString().padStart(2,0);
-    const minutes = now.getMinutes().toString().padStart(2,0);
-    const seconds = now.getSeconds().toString().padStart(2,0);
+    const hours = now.getHours().toString().padStart(2,"0");
+    const minutes = now.getMinutes().toString().padStart(2,"0");
+    const seconds = now.getSeconds().toString().padStart(2,"0");
     const timeString = `${hours}:${minutes}:${seconds}`;
     document.getElementById("clock").textContent=timeString;
 
 
-    const options = {weekday:"long", year:"numeric", month: "long", day:"numeric", }
+    const options = {weekday: "long", year: "numeric", month: "long", day:"numeric"};
     const dateString = now.toLocaleDateString(undefined, options);
+
+    document.getElementById("date").textContent= dateString;
 }
 
 updateClock();
